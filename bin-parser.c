f67d2a306ff365cc1f5d269f9af1f26e53abaebb
DeletedUser1836 <den.pylypen@example.com>
2025-07-27 09:57:44 +0200

    parse args maybe??

--- bin-parser.c
+++ bin-parser.c
@@ -1,11 +1,63 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+/*
+ How this shit has to work:
+ 
+ user@pc$~ ./bin-parse "01100001 01100111 01100111 01101001 01101110 00001010"
+ aggin
+ user@pc$~ 
+
+ then from file
+
+ user@pc$~ ./bin-parse -f bin-file.txt
+ <things he has in this file idk>
+ user@pc$~
+
+ and help menu
+ 
+ user@pc$~ ./bin-parse -h
+ <help-message>
+ user@pc$~
+*/
 
 int main(int argc, char* argv[])
 {
-    for(int i = argc; i != 0; i--)
+    //Programm modes
+    bool helpMode = false;
+    bool fileMode = false;
+    bool commonMode = false;
+
+    //parse args
+    for(int i = argc; i > 0; i--)
     {
-        printf("\n Argument %d is argv[%d] and it is %s", i, i, argv[i]);
+        if(argv[i] == "-f")
+        {
+            fileMode = true;
+        }
+        else if(argv[i] == "-h")
+        {
+            helpMode = true;
+        }
+        else
+        {
+            commonMode = true;
+        }
+        
     }
-    return 0;
+    if(argc > 1)
+    {    
+        char bin_msg[] = argv[1];
+    }
+    else if(argc == 1)
+    {
+        char bin_msg[] = argv[0];
+    }
+    else if(argc == 0)
+    {
+        printf("\nText to decode was not provided/Too few argumets.\n");
+        printf("Please run %c./bin-parse -h%c for help. \n", 34,  34);
+    }
 }
