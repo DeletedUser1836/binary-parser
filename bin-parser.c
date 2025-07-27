6ffe1f4950eda4ea408570fe0c76eef1aa2907cd
DeletedUser1836 <den.pylypen@example.com>
2025-07-27 09:27:49 +0200

    idk how that works tbh, some tests of the lib ig

--- bin-parser.c
+++ bin-parser.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-    
+    for(int i = argc; i != 0; i--)
+    {
+        printf("\n Argument %d is argv[%d] and it is %s", i, i, argv[i]);
+    }
+    return 0;
 }
